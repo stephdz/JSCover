89faca165532e5bac2db057336b1899896dc1854
tntim96 <tntim96@gmail.com>
2020-09-26 20:07:25 +1000

    Add basic JaCoCo XML code infrastructure (WIP)

--- src/test-integration/java/jscover/report/jacocoxml/JaCoCoXmlGeneratorIntegrationTest.java
+++ src/test-integration/java/jscover/report/jacocoxml/JaCoCoXmlGeneratorIntegrationTest.java
@@ -8,10 +8,11 @@ import static jscover.report.coberturaxml.CoberturaXmlGeneratorTest.parseXml;
 public class JaCoCoXmlGeneratorIntegrationTest {
     private JSONDataMerger jsonDataMerger = new JSONDataMerger();
 
-    private String validXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+    private String validXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
             "<!DOCTYPE report PUBLIC \"-//JACOCO//DTD Report 1.1//EN\" \"report.dtd\">\n" +
             "\n" +
-            "<report name=\"JSCover\"></report>";
+            "<report name=\"JSCover\">\n" +
+            "</report>";
 
     @Test
     public void shouldValidateXmlToDtd() throws Exception {
